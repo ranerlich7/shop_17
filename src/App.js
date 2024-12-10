6b8015b9370a2809a8e36748be28968d7c0a2e49
Ran Erlich <ranerlich@gmail.com>
2024-12-10 21:27:39 +0530

    added design for product + useeffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import axios from "axios";
-import "./App.css";
-import { useState } from "react";
+import axios from 'axios'
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [products, setProducts] = useState([])
+  
+  useEffect(() => {
+    getProducts()
+  }, [])
 
   function getProducts() {
-    axios.get("http://localhost:3006/products").then((response) => {
-      console.log(response.data);
+    axios.get('http://localhost:3006/products').then((response) => {
       setProducts(response.data)
-    });
+    })
   }
+  
   return (
     <>
-      <button onClick={() => getProducts()}>Show Products</button>
-      <div>
-      {products.map(product => <li>{product.name}</li>)}
+      <div className='App'>
+        <div className='product-container'>
+          {products.map((product, index) => (
+            <div key={index} className='product-card'>
+              <img src={product.image} alt={product.name} className='product-image' />
+              <h3 className='product-name'>{product.name}</h3>
+              <p className='product-price'>${product.price.toFixed(2)}</p>
+            </div>
+          ))}
+        </div>
       </div>
-
     </>
-  );
+  )
 }
 
-export default App;
+export default App
